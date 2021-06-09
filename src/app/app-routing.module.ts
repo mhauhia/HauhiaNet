e1c3c8b1758e948369badf3a736663c8d8d82967
mhauhia <markus.hauhia@gmail.com>
2021-06-09 19:59:42 +0300

    add components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactComponent } from './contact/contact.component';
+import { CVComponent } from './cv/cv.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+// Määritellään linkit sivuille
+// Path = hauhia.net/path
+const routes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent,
+  },
+
+  {
+    path: 'cv',
+    component: CVComponent,
+  },
+
+  {
+    path: 'contact',
+    component: ContactComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PersonalPageComponent } from './personal-page/personal-page.component';
+import { CVComponent } from './cv/cv.component';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PersonalPageComponent
+    PersonalPageComponent,
+    HomeComponent,
+    CVComponent,
+    ContactComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
