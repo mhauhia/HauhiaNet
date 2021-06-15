eaac9397935553e12bdc19928b1fde58e33b053c
mhauhia <markus.hauhia@gmail.com>
2021-06-15 11:22:04 +0300

    modify app component main page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,19 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { PersonalPageComponent } from './personal-page/personal-page.component';
+
 import { CVComponent } from './cv/cv.component';
 import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    PersonalPageComponent,
-    HomeComponent,
-    CVComponent,
-    ContactComponent,
-  ],
+  declarations: [AppComponent, HomeComponent, CVComponent, ContactComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
