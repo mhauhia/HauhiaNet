1adc328e421d39484820c793b9355f1f2ab6a0d1
mhauhia <markus.hauhia@gmail.com>
2021-06-15 21:27:39 +0300

    add latest changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, HostListener, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'HauhiaNet';
+
+  //Tests the ngIf in app-component template
+  isTruthy: boolean = false;
+  viewSection() {
+    this.isTruthy = true;
+    console.log(this.isTruthy);
+  }
+
+  //Array to background styles
+  backgroundImages = [
+    '.background-image-1',
+    '.background-image-3',
+    '.background-image-4',
+  ];
+  constructor() {}
 }
